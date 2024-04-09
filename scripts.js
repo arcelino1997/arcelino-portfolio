6ccc0db8d7e817543adfe5cf3b40b6d7690a1a35
Arcelino <a.santos.contact@gmail.com>
2024-04-09 18:47:12 -0300

    atualizando body e home

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+function inicio(){
+    document.querySelector('#home').style.display = 'flex';
+    document.querySelector('#sobre__mim').style.display = 'none';
+    document.querySelector('#formacao').style.display = 'none';
+    document.querySelector('#hobbies').style.display = 'none';
+    document.querySelector('#projetos').style.display = 'none';
+    document.querySelector('#contato').style.display = 'none';
+}
 function about(){
     document.querySelector('#home').style.display = 'none';
     document.querySelector('#sobre__mim').style.display = 'flex';
