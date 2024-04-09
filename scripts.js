a861623b9ba26d92241dde2e87064649a70d826c
Arcelino <a.santos.contact@gmail.com>
2024-04-09 10:23:19 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,46 @@
+function about(){
+    document.querySelector('#home').style.display = 'none';
+    document.querySelector('#sobre__mim').style.display = 'flex';
+    document.querySelector('#formacao').style.display = 'none';
+    document.querySelector('#hobbies').style.display = 'none';
+    document.querySelector('#projetos').style.display = 'none';
+    document.querySelector('#contato').style.display = 'none';
+    let el = document.querySelector('#sobre__mim');
+    el.style.flexDirection = 'column';
+}
+function formacao(){
+    document.querySelector('#home').style.display = 'none';
+    document.querySelector('#sobre__mim').style.display = 'none';
+    document.querySelector('#formacao').style.display = 'block';
+    document.querySelector('#hobbies').style.display = 'none';
+    document.querySelector('#projetos').style.display = 'none';
+    document.querySelector('#contato').style.display = 'none';
+}
+function hobbies(){
+    document.querySelector('#home').style.display = 'none';
+    document.querySelector('#sobre__mim').style.display = 'none';
+    document.querySelector('#formacao').style.display = 'none';
+    document.querySelector('#hobbies').style.display = 'block';
+    document.querySelector('#projetos').style.display = 'none';
+    document.querySelector('#contato').style.display = 'none';
+}
+function projetos(){
+    document.querySelector('#home').style.display = 'none';
+    document.querySelector('#sobre__mim').style.display = 'none';
+    document.querySelector('#formacao').style.display = 'none';
+    document.querySelector('#hobbies').style.display = 'none';
+    document.querySelector('#projetos').style.display = 'block';
+    document.querySelector('#contato').style.display = 'none';
+}
+function contato(){
+    document.querySelector('#home').style.display = 'none';
+    document.querySelector('#sobre__mim').style.display = 'none';
+    document.querySelector('#formacao').style.display = 'none';
+    document.querySelector('#hobbies').style.display = 'none';
+    document.querySelector('#projetos').style.display = 'none';
+    document.querySelector('#contato').style.display = 'block';
+}
+function email(){
+    const Email = document.querySelector('#email').innerText;
+      navigator.clipboard.writeText(Email);
+  }
